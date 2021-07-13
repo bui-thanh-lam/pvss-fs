30e7ce52f4f4307fd4447e976d3fa509f68769c2
bui-thanh-lam <lamthientaihehe@gmail.com>
2021-07-13 16:17:59 +0700

    update gitignore

---
+++ src/pvssfs/config.py
@@ -0,0 +1,3 @@
+CLIENT_LIB_PATH = "/home/lam-bui/Documents/Projects/ATTT/BTL/pvss-fs/src/cpp_libs/Client_Lib.so"
+TEST_DOCUMENT_PATH = "/home/lam-bui/Documents/Projects/ATTT/BTL/pvss-fs/test/test.txt"
+TEST_DECRYPTED_DOC_PATH = "/home/lam-bui/Documents/Projects/ATTT/BTL/pvss-fs/test/test.bin"
