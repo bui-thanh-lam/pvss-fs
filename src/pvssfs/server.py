79f05e0c5c35db9b298a8bf196237cc743bcca4c
phamnam1805 <57164967+phamnam1805@users.noreply.github.com>
2021-07-14 11:35:26 +0700

    Tạo struct

--- src/pvssfs/server.py
+++ src/pvssfs/server.py
@@ -1,1 +1,23 @@
 from communication import ServerCommunicator
+import ctypes
+from config import SERVER_LIB_PATH
+import os
+
+
+class KeyComponent(ctypes.Structure):
+    _fields_ = [('k', ctypes.c_char_p), ('x', ctypes.c_int)]
+
+
+class KeySharing(ctypes.Structure):
+    _fields_ = [('N', ctypes.c_int), ('T', ctypes.c_int), ('p', ctypes.c_char_p), ('key_component', KeyComponent)]
+
+
+class ServerHandler:
+
+    def __init__(self):
+        # load lib
+        _path = os.path.join(SERVER_LIB_PATH)
+        _mod = ctypes.cdll.LoadLibrary(_path)
+
+        #
+
