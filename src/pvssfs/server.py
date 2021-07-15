8adaebc083aa0057e2d86203ac05ac1078b078af
phamnam1805 <57164967+phamnam1805@users.noreply.github.com>
2021-07-15 08:51:29 +0700

    Update server.py

--- src/pvssfs/server.py
+++ src/pvssfs/server.py
@@ -1,6 +1,7 @@
 import os
 import ctypes
 import config
+import pprint
 
 
 class KeyComponent(ctypes.Structure):
@@ -8,12 +9,16 @@ class KeyComponent(ctypes.Structure):
 
 
 class KeySharing(ctypes.Structure):
-    _fields_ = [('N', ctypes.c_int), ('T', ctypes.c_int), ('p', ctypes.c_char_p), ('key_component', ctypes.POINTER(KeyComponent))]
+    _fields_ = [('N', ctypes.c_int), ('T', ctypes.c_int), ('p', ctypes.c_char_p),
+                ('key_component', ctypes.POINTER(KeyComponent))]
 
 
 class ServerHandler:
 
     def __init__(self):
+        # init client_id
+        self.current_client_id = -1
+
         # load lib
         _mod = ctypes.cdll.LoadLibrary(config.SERVER_LIB_PATH)
 
@@ -41,7 +46,55 @@ class ServerHandler:
         S = ctypes.c_char_p(S.encode("utf-8"))
         N = ctypes.c_int(N)
         T = ctypes.c_int(T)
-        return self.key_sharing_phase(S, N, T)
+        shares = self.convert_key_sharing_to_json_form(self.key_sharing_phase(S, N, T))
+        return shares
+
+    def convert_key_sharing_to_json_form(self, key_sharing):
+        """Convert the key_sharing (struct KeySharing) to json form
+
+               Args:
+                   key_sharing: KeySharing
+
+               Return:
+                   shares(json form): {"N":"", "T":"", "p":"","key_components":[{"x":"","k":""},..]}
+               """
+        shares = {}
+        shares["N"] = key_sharing.N
+        shares["T"] = key_sharing.T
+        shares["p"] = key_sharing.p.decode("utf-8")
+        key_components = []
+        for i in range(0,key_sharing.N):
+            key_component = {}
+            key_component["x"] = key_sharing.key_component[i].x
+            key_component["k"] = key_sharing.key_component[i].k.decode("utf-8")
+            key_components.append(key_component)
+        shares["key_components"] = key_components
+        return shares
+
+
+    def convert_shares_to_key_sharing(self, shares):
+        """Convert the shares (json form) to struct KeySharing
+
+                       Args:
+                           shares(json form): {"N":"", "T":"", "p":"","key_components":[{"x":"","k":""},..]}
+
+                       Return:
+                           key_sharing: KeySharing
+            """
+        N = shares["N"]
+        T = shares["T"]
+        p = shares["p"]
+        key_component_array = ctypes.POINTER(KeyComponent)
+        key_components = shares["key_components"]
+        for i in range(0,T):
+            key_component = KeyComponent(ctypes.c_char_p(key_components[i]["k"].encode("utf-8")), ctypes.c_int(key_components[i]["x"]))
+            key_component_array(key_component)
+        print(key_component_array)
+        for i in range(0,T):
+            print(key_component_array[i].x)
+        # key_sharing = KeySharing(ctypes.c_int(N),ctypes.c_int(T),ctypes.c_char_p(p.encode("utf-8")),key_component_array
+        key_sharing = ""
+        return key_sharing
 
     def reconstruct_key(self, shares):
         """Reconstruct the secret key from collected shares
@@ -52,21 +105,24 @@ class ServerHandler:
         Return:
             key (str): recontructed key
         """
-        return self.key_reconstruction_phase(shares)
-    
+        return self.key_reconstruction_phase(self.convert_shares_to_key_sharing(shares))
+
     def collect_shares(self):
         pass
-    
+
     def distribute_share(self, share, shareholder_id):
         pass
 
+    def distribute_client_id(self):
+        self.current_client_id += 1
+        print(self.current_client_id)
+        return self.current_client_id
 
 server = ServerHandler()
 S = "3CE7C3C862457688D415D34753A446D0"
 N = 10
 T = 5
 shares = server.compute_shares(S, N, T)
-print(shares)
+pprint.pprint(shares)
 reconstructed_key = server.reconstruct_key(shares)
 print(reconstructed_key)
-
