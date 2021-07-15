a62fe12b08327ec8d166bc50e081808fe02042d4
phamnam1805 <57164967+phamnam1805@users.noreply.github.com>
2021-07-15 08:51:16 +0700

    Update client.py

--- src/pvssfs/client.py
+++ src/pvssfs/client.py
@@ -1,7 +1,7 @@
 import os
 import config
 import requests
-from ctypes import *
+import ctypes
 
 
 class ClientHandler:
@@ -14,19 +14,25 @@ class ClientHandler:
     """
 
     def __init__(self):
+
+        # self.client_id = self.get_client_id()
+        self.client_id = requests.get("http://localhost:8001/get_client_id").content.decode("utf-8")
+
         # load lib
         _path = os.path.join(config.CLIENT_LIB_PATH)
-        _mod = cdll.LoadLibrary(_path)
+        _mod = ctypes.cdll.LoadLibrary(_path)
 
         # char *Encrypt_File(char *input, char *output)
         self.encryptor = _mod.Encrypt_File
-        self.encryptor.argtypes = (c_char_p, c_char_p)
-        self.encryptor.restype = c_char_p
+        self.encryptor.argtypes = (ctypes.c_char_p, ctypes.c_char_p)
+        self.encryptor.restype = ctypes.c_char_p
 
         # void Decrypt_File(char* input, char* output, char* key_c)
         self.decryptor = _mod.Decrypt_File
-        self.decryptor.argtypes = (c_char_p, c_char_p, c_char_p)
-        self.decryptor.restype = c_void_p
+        self.decryptor.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p)
+        self.decryptor.restype = ctypes.c_void_p
+
+
 
     def encrypt_file(self, plain_file_path, cipher_file_path):
         """Encrypt file by AES in CTR mode
@@ -38,9 +44,12 @@ class ClientHandler:
             Return:
                 key (str): the aes key in hex code
         """
-        plain_file_path = c_char_p(plain_file_path.encode("utf-8"))
-        cipher_file_path = c_char_p(cipher_file_path.encode("utf-8"))
-        return self.encryptor(plain_file_path, cipher_file_path).decode("utf-8")
+        plain_file_name = os.path.basename(plain_file_path)
+        cipher_file_name = os.path.basename(cipher_file_path)
+        plain_file_path = ctypes.c_char_p(plain_file_path.encode("utf-8"))
+        cipher_file_path = ctypes.c_char_p(cipher_file_path.encode("utf-8"))
+        key = self.encryptor(plain_file_path, cipher_file_path).decode("utf-8")
+        self.send_key(key, plain_file_name, cipher_file_name)
 
     def decrypt_file(self, cipher_file_path, plain_file_path, key):
         """Decrypt file by AES in CTR mode
@@ -50,13 +59,20 @@ class ClientHandler:
                 plain_file_path (str): the path to desired plain file after decryption
                 key (str): the a AES key in hex code
         """
-        cipher_file_path = c_char_p(cipher_file_path.encode("utf-8"))
-        plain_file_path = c_char_p(plain_file_path.encode("utf-8"))
-        key = c_char_p(key.encode("utf-8"))
+
+        cipher_file_path = ctypes.c_char_p(cipher_file_path.encode("utf-8"))
+        plain_file_path = ctypes.c_char_p(plain_file_path.encode("utf-8"))
+        key = ctypes.c_char_p(key.encode("utf-8"))
         return self.decryptor(cipher_file_path, plain_file_path, key)
 
-    def send_key(self, key):
-        pass
+    def send_key(self, key, plain_file_name, cipher_file_name):
+        msg = {}
+        msg["key"] = key
+        msg["client_id"] = self.client_id
+        msg["plain_file_name"] = plain_file_name
+        msg["cipher_file_name"] = cipher_file_name
+        print(msg)
+
 
     def send_share(self, share):
         pass
@@ -73,10 +89,10 @@ class ClientHandler:
     
 
 
-# client = ClientHandler()
-# key = client.encrypt_file(
-#     config.TEST_DOCUMENT_PATH,
-#     config.TEST_DECRYPTED_DOC_PATH
-# )
-# print(key)
+client = ClientHandler()
+key = client.encrypt_file(
+    config.TEST_DOCUMENT_PATH,
+    config.TEST_DECRYPTED_DOC_PATH
+)
+
 # client.decrypt_file(config.TEST_DECRYPTED_DOC_PATH, config.TEST_RECOVERED_DOC_PATH, key)
